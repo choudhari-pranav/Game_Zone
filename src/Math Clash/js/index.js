4e5bd78a0eb64b441fab30a4c4d846c7bb773036
Pranav <Pranavchoudhari.comp@gmail.com>
2023-01-16 11:47:01 +0530

    Updated code

--- src/Math Clash/js/index.js
+++ src/Math Clash/js/index.js
@@ -1,4 +1,3 @@
-// ---------------------
 let dimenstions = 6;
 let gameOver = false;
 let board = [];
@@ -28,250 +27,40 @@ let musicAudio = new Audio();
 musicAudio.src = "./sound/music.mp3";
 musicAudio.volume = 0.2;
 
-
 const playMusic = (swtch) => {
-  if(swtch) {
+  if (swtch) {
     musicAudio.play();
   } else {
     musicAudio.pause();
   }
-  musicAudio.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-  }, swtch);
-  //musicAudio.play();
+  musicAudio.addEventListener(
+    "ended",
+    function () {
+      this.currentTime = 0;
+      this.play();
+    },
+    swtch
+  );
 };
 
 let musicOn = false;
 
 const checkMusic = () => {
-  if(musicOn) {
-    document.getElementById('stop').classList.add('hide');
-    document.getElementById('play').classList.remove('hide');
+  if (musicOn) {
+    document.getElementById("stop").classList.add("hide");
+    document.getElementById("play").classList.remove("hide");
     playMusic(false);
     musicOn = false;
   } else {
-    document.getElementById('play').classList.add('hide');
-    document.getElementById('stop').classList.remove('hide');
+    document.getElementById("play").classList.add("hide");
+    document.getElementById("stop").classList.remove("hide");
     playMusic(true);
     musicOn = true;
   }
 };
 
-
-let musicBtn = document.getElementById('music_btn');
-musicBtn.addEventListener('click', checkMusic);
-
-
-const removeAlert = () => {
-  let alert = document.getElementById('alert');
-  document.body.removeChild(alert);
-};
-
-const createAlert = (txt) => {
-  let newAlert = document.createElement('div');
-  newAlert.classList.add('alert');
-  newAlert.setAttribute('id', 'alert');
-  let newAlertBody = document.createElement('div');
-  newAlertBody.classList.add('alert-body');
-  newAlertBody.innerHTML = txt;
-  let newBtn = document.createElement('button');
-  newBtn.innerHTML = "Close";
-  newBtn .classList.add('custom-btn');
-  newBtn .classList.add('mt-3');
-  newBtn.addEventListener('click', () => removeAlert());
-
-
-  newAlertBody.appendChild(newBtn);
-  newAlert.appendChild(newAlertBody);
-  return newAlert;
-};
-
-const showAlert = (txt) => {
-  let getAlert = createAlert(txt);
-  document.body.appendChild(getAlert);
-};
-
-
-const getTarget = () => {
-  return Math.ceil(10 + Math.random() * 60);
-};
-
-let target;
-
-const updateTarget = () => {
-  target = getTarget();
-  document.getElementById("target").innerHTML = target;
-};
-
-const updateCurrentSum = () => {
-  document.getElementById("current_sum").innerHTML = currentSum;
-};
-
-const updateScore = () => {
-  document.getElementById("score").innerHTML = countScore;
-};
-
-const updateBestScore = () => {
-  document.getElementById("best_score").innerHTML = bestScore;
-};
-
-const removeAll = () => {
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (board[i][j].selected) {
-        board[i][j].value = "";
-        board[i][j].selected = !board[i][j].selected;
-      }
-    }
-  }
-};
-
-const deselectAll = () => {
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (board[i][j].selected) {
-        board[i][j].selected = false;
-      }
-    }
-  }
-};
-// --------
-const handleClick = (cell) => {
-  if (cell.innerText === "" || gameOver) {
-    // add gameOver too here
-    return;
-  }
-
-  let i = parseInt(cell.id[0]);
-  let j = parseInt(cell.id[1]);
-  //console.log(i, j); // improve this method
-  board[i][j].selected = !board[i][j].selected;
-
-  if (board[i][j].selected) {
-    cell.classList.add("selected");
-    currentSum += parseInt(cell.innerHTML);
-  } else if (cell.classList.contains("selected")) {
-    cell.classList.remove("selected");
-    currentSum -= parseInt(cell.innerHTML);
-  }
-
-  checkSum();
-  updateCurrentSum();
-  updateBoard();
-  clickAudio.play()
-};
-
-const createCells = () => {
-  let allCells = document.getElementById("all_cell");
-  for (let i = 0; i < dimenstions; i++) {
-    let row = document.createElement("div");
-    row.setAttribute("class", "row anti-row");
-    for (let j = 0; j < dimenstions; j++) {
-      let cell = document.createElement("div");
-      cell.id = `${i}${j}`;
-      cell.setAttribute("class", "cell");
-      cell.addEventListener("click", (event) => handleClick(cell));
-      row.appendChild(cell);
-    }
-    allCells.appendChild(row);
-  }
-};
-
-// ----------------
-
-const generateRandomValues = () => {
-  let generatedValues = [];
-  for (let i = 0; i < dimenstions; i++) {
-    let obj = {};
-    obj["value"] = Math.ceil(Math.random() * 9);
-    obj["selected"] = false;
-    generatedValues.push(obj);
-  }
-  board.unshift(generatedValues);
-};
-
-const updateBoard = () => {
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      let cell = document.getElementById(`${i}${j}`);
-      cell.innerHTML = board[i][j].value;
-
-      if (board[i][j].selected) {
-        cell.classList.add("selected");
-      } else if (cell.classList.contains("selected")) {
-        cell.classList.remove("selected");
-      }
-    }
-  }
-};
-
-const checkSum = () => {
-  if (currentSum === target) {
-    removeAll();
-    updateTarget();
-    countScore += currentSum;
-    bestScore = Math.max(countScore, bestScore);
-    updateBestScore();
-    updateScore();
-    currentSum = 0;
-    winAudio.play();
-  } else if (currentSum > target) {
-    deselectAll();
-    updateBoard();
-    currentSum = 0;
-  }
-};
-
-const isGameOver = () => {
-  if (board.length !== rowSize) {
-    return false;
-  }
-
-  //console.log("in GameOver to check");
-  for (let i = 0; i < rowSize; i++) {
-    if (board[rowSize - 1][i].value !== "") {
-      deselectAll();
-      document.getElementById("try_btn").classList.remove("hide"); // enable try btn
-      return true;
-    }
-  }
-
-  //console.log("Poping empty row");
-  board.pop();
-  colSize -= 1;
-
-  return false;
-};
-
-let intervalId;
-
-const startTime = (levelTime) => {
-  intervalId = setInterval(() => {
-    generateRandomValues();
-    // console.log(`Col size : ${colSize}`);
-    colSize += 1;
-    updateBoard();
-
-    if (isGameOver()) {
-      gameOver = true;
-      clearInterval(intervalId);
-      playMusic(false);
-      gameOverAudio.play();
-      showAlert("Game Over!");
-      return;
-    }
-  }, levelTime * 1000); // time as per level
-};
-
-const delayAtStart = (level_time) => {
-  setTimeout(() => {
-    generateRandomValues();
-    colSize += 1;
-    updateBoard();
-    startTime(level_time);
-  }, 3000); // three second Delay
-};
+let musicBtn = document.getElementById("music_btn");
+musicBtn.addEventListener("click", checkMusic);
 
 const startGame = () => {
   if (gameOver) {
@@ -302,23 +91,175 @@ const startGame = () => {
   checkMusic();
 };
 
-// add event listener to try again btn
+const showAlert = (txt) => {
+  let getAlert = createAlert(txt);
+  document.body.appendChild(getAlert);
+};
 
-document
-  .getElementById("try_btn")
-  .addEventListener("click", () => enableTryAgain());
+const createAlert = (txt) => {
+  let newAlert = document.createElement("div");
+  newAlert.classList.add("alert");
+  newAlert.setAttribute("id", "alert");
+  let newAlertBody = document.createElement("div");
+  newAlertBody.classList.add("alert-body");
+  newAlertBody.innerHTML = txt;
+  let newBtn = document.createElement("button");
+  newBtn.innerHTML = "Close";
+  newBtn.classList.add("custom-btn");
+  newBtn.classList.add("mt-3");
+  newBtn.addEventListener("click", () => removeAlert());
 
-const emptyAllCells = () => {
-  for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < 6; j++) {
+  newAlertBody.appendChild(newBtn);
+  newAlert.appendChild(newAlertBody);
+  return newAlert;
+};
+
+const removeAlert = () => {
+  let alert = document.getElementById("alert");
+  document.body.removeChild(alert);
+};
+
+const createCells = () => {
+  let allCells = document.getElementById("all_cell");
+  for (let i = 0; i < dimenstions; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row anti-row");
+    for (let j = 0; j < dimenstions; j++) {
+      let cell = document.createElement("div");
+      cell.id = `${i}${j}`;
+      cell.setAttribute("class", "cell");
+      cell.addEventListener("click", (event) => handleClick(cell));
+      row.appendChild(cell);
+    }
+    allCells.appendChild(row);
+  }
+};
+
+const handleClick = (cell) => {
+  if (cell.innerText === "" || gameOver) {
+    // add gameOver too here
+    return;
+  }
+
+  let i = parseInt(cell.id[0]);
+  let j = parseInt(cell.id[1]);
+  //console.log(i, j); // improve this method
+  board[i][j].selected = !board[i][j].selected;
+
+  if (board[i][j].selected) {
+    cell.classList.add("selected");
+    currentSum += parseInt(cell.innerHTML);
+  } else if (cell.classList.contains("selected")) {
+    cell.classList.remove("selected");
+    currentSum -= parseInt(cell.innerHTML);
+  }
+
+  checkSum();
+  updateCurrentSum();
+  updateBoard();
+  clickAudio.play();
+};
+
+const updateCurrentSum = () => {
+  document.getElementById("current_sum").innerHTML = currentSum;
+};
+
+const checkSum = () => {
+  if (currentSum === target) {
+    removeAll();
+    shiftValues();
+    updateTarget();
+    countScore += currentSum;
+    bestScore = Math.max(countScore, bestScore);
+    updateBestScore();
+    updateScore();
+    currentSum = 0;
+    winAudio.play();
+  } else if (currentSum > target) {
+    deselectAll();
+    updateBoard();
+    currentSum = 0;
+  }
+};
+
+const removeAll = () => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j].selected) {
+        board[i][j].value = "";
+        board[i][j].selected = !board[i][j].selected;
+      }
+    }
+  }
+};
+
+let target;
+
+const updateTarget = () => {
+  target = getTarget();
+  document.getElementById("target").innerHTML = target;
+};
+
+const getTarget = () => {
+  return Math.ceil(10 + Math.random() * 60);
+};
+
+const updateScore = () => {
+  document.getElementById("score").innerHTML = countScore;
+};
+
+const updateBestScore = () => {
+  document.getElementById("best_score").innerHTML = bestScore;
+};
+
+const updateBoard = () => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
       let cell = document.getElementById(`${i}${j}`);
-      cell.innerHTML = "";
-      if (cell.classList.contains("selected")) {
+      cell.innerHTML = board[i][j].value;
+
+      if (board[i][j].selected) {
+        cell.classList.add("selected");
+      } else if (cell.classList.contains("selected")) {
         cell.classList.remove("selected");
       }
     }
   }
 };
+
+const isGameOver = () => {
+  if (board.length !== rowSize) {
+    return false;
+  }
+
+  //console.log("in GameOver to check");
+  for (let i = 0; i < rowSize; i++) {
+    if (board[rowSize - 1][i].value !== "") {
+      deselectAll();
+      document.getElementById("try_btn").classList.remove("hide"); // enable try btn
+      return true;
+    }
+  }
+
+  //console.log("Poping empty row");
+  board.pop();
+  colSize -= 1;
+  return false;
+};
+
+const deselectAll = () => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j].selected) {
+        board[i][j].selected = false;
+      }
+    }
+  }
+};
+
+document
+  .getElementById("try_btn")
+  .addEventListener("click", () => enableTryAgain());
 
 const enableTryAgain = () => {
   board = [];
@@ -340,4 +281,109 @@ const enableTryAgain = () => {
   document.getElementById("try_btn").classList.add("hide"); // disable try btn
   playMusic(true);
   checkMusic();
+};
+
+const emptyAllCells = () => {
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 6; j++) {
+      let cell = document.getElementById(`${i}${j}`);
+      cell.innerHTML = "";
+      if (cell.classList.contains("selected")) {
+        cell.classList.remove("selected");
+      }
+    }
+  }
+};
+
+
+// --------
+
+
+
+
+// ----------------
+
+const generateRandomValues = () => {
+  let generatedValues = [];
+  for (let i = 0; i < dimenstions; i++) {
+    let obj = {};
+    obj["value"] = Math.ceil(Math.random() * 9);
+    obj["selected"] = false;
+    generatedValues.push(obj);
+  }
+  board.unshift(generatedValues);
+};
+
+
+
+
+
+
+
+let intervalId;
+const startTime = (levelTime) => {
+  intervalId = setInterval(() => {
+    generateRandomValues();
+    // console.log(`Col size : ${colSize}`);
+    colSize += 1;
+    updateBoard();
+
+    if (isGameOver()) {
+      gameOver = true;
+      clearInterval(intervalId);
+      playMusic(false);
+      gameOverAudio.play();
+      showAlert("Game Over!");
+      return;
+    }
+  }, levelTime * 1000); // time as per level
+};
+
+
+
+
+// const shiftValues = () => {
+//   //console.log(`In newShift Colsize ${colSize}`);
+//   for (let i = 0; i < colSize; i++) {
+//     if (board[i].every((el) => el === "")) {
+//       //console.log(`Poping this row ${i}`);
+//       board.splice(i, 1); // remove empty rows first
+//       colSize -= 1;
+//     }
+//   }
+
+//   const shift = (j, i) => {
+//     //console.log(`Clear here ${i}, ${j}`);
+//     if (j < colSize) {
+//       let x = j;
+//       while (board[j - 1][i].value === "" && j < colSize) {
+//         //console.log(`Shifting here ${j-1}, ${i} getting val from ${j} ${i}`);
+//         //console.log(`Value : ${board[j][i].value}`);
+//         let m = j;
+//         while (board[m - 1][i].value === "" && m < colSize) {
+//           m++;
+//         }
+//         //console.log(`Vlaue of M ${m}`);
+//         board[j - 1][i].value = board[m - 1][i].value;
+//         board[m - 1][i].value = "";
+//         j += 1;
+//       }
+//       shift(x + 1, i);
+//     } else if (i + 1 < board[0].length) {
+//       //console.log(`Changing Col here ${i+1} passing`);
+//       shift(1, i + 1);
+//     } else {
+//       return;
+//     }
+//   };
+//   shift(1, 0); // j is row and i is col
+// };
+
+const delayAtStart = (level_time) => {
+  setTimeout(() => {
+    generateRandomValues();
+    colSize += 1;
+    updateBoard();
+    startTime(level_time);
+  }, 3000); // three second Delay
 };
--- src/tic-tac-toe/js/index.js
+++ src/tic-tac-toe/js/index.js
@@ -8,7 +8,7 @@ let no_of_grid = 0;
 let grid = document.getElementById('grid');
 let celebrate = document.getElementById('celebrate_btn');
 
-let playMode = document.getElementById('play_mode');
+// let playMode = document.getElementById('play_mode');
 // let mode;
 // playMode.addEventListener('change', (event) => {
 // 	// mode = event.target.value;
@@ -19,6 +19,91 @@ let playMode = document.getElementById('play_mode');
 
 let gameOver = false;
 let turnElement = document.getElementById("turn");
+
+const start = () => {
+  player_1 = document.getElementById("player_1").value;
+  player_2 = document.getElementById("player_2").value;
+
+  if (player_1.length === 0 || player_2.length === 0) {
+    alert("Please select name of players!");
+    return;
+  }
+
+  if (player_1 === player_2) {
+    alert("Players Names should be different!");
+    return;
+  }
+
+  players.push(player_1);
+  players.push(player_2);
+
+  let no_of_grid_str = document.getElementById("no_of_grid").value;
+  no_of_grid = parseInt(no_of_grid_str);
+
+
+  //console.log(no_of_grid);
+
+  if (no_of_grid > 10) {
+    alert("10 is the Max limit to create Grid!");
+    return;
+  }
+  if (no_of_grid < 3) {
+    alert("3 is the Minimum limit to create Grid!");
+    return;
+  }
+  if (isNaN(no_of_grid)) {
+    alert("Please provide valid Grid No. !");
+    return;
+  }
+
+  if (no_of_grid <= 3) {
+    if (screen.width <= 420) {
+      he_wi = "80px";
+    } else {
+      he_wi = "120px";
+    }
+  } else if (no_of_grid > 3 && no_of_grid <= 5) {
+    if (screen.width <= 480) {
+      he_wi = "50px";
+    } else {
+      he_wi = "80px";
+    }
+  } else if (no_of_grid > 5 && no_of_grid <= 10) {
+    if (screen.width <= 424) {
+      if (no_of_grid > 8) {
+        alert("8 is the Max limit to create Grid! (for your device)");
+        return;
+      }
+      //console.log("everything good here! 424");
+      he_wi = "35px";
+    } else if (screen.width <= 480) {
+      he_wi = "30px";
+      //console.log("everything good here! 480");
+    } else if (screen.width <= 770) {
+      he_wi = "40px";
+      //console.log("everything good here! 770");
+    } else {
+      //console.log("everything good here! else");
+      he_wi = "50px";
+    }
+  }
+
+  //console.log(players);
+  // hide form
+  document.body.style.backgroundImage = "none";
+  document.getElementById("form_container").setAttribute("class", "hide");
+
+  createCells();
+  createBoard();
+
+  document.getElementById("turn").innerHTML = players[turn % 2];
+  document.getElementById("player_1").disabled = "true";
+  document.getElementById("player_2").disabled = "true";
+  document.getElementById("no_of_grid").disabled = "true";
+  document.getElementById("start_btn").disabled = "true";
+  document.getElementById("hide_section").className = "";
+//   console.log("Calling Computer function");
+};
 
 const createCells = () => {
 	for(let i=0; i<no_of_grid; i++) {
@@ -37,155 +122,75 @@ const createCells = () => {
 	}
 };
 
-const start = () => {
-	player_1 = document.getElementById('player_1').value;
-	player_2 = document.getElementById('player_2').value;
-
-	if(player_1.length === 0 || player_2.length === 0) {
-		alert("Please select name of players!");
-		return;
-	}
-
-	if(player_1 === player_2) {
-		alert("Players Names should be different!");
-		return;
-	}
-
-	let no_of_grid_str = document.getElementById('no_of_grid').value;
-	no_of_grid = parseInt(no_of_grid_str);
-
-	players.push(player_1);
-	players.push(player_2);
-	
-	//console.log(no_of_grid);
-
-	if(no_of_grid > 10) {
-		alert("10 is the Max limit to create Grid!");
-		return;
-	}
-	if(isNaN(no_of_grid)) {
-		alert("Please provide valid Grid No. !");
-		return;
-	}
-
-
-	if(no_of_grid <= 3) {
-		if(screen.width <= 420) {
-			he_wi = '80px';
-		} else {
-			he_wi = '120px';
-		}
-	} else if(no_of_grid >3 && no_of_grid <= 5) {
-		if(screen.width <= 480) {
-			he_wi = '50px';
-		} else {
-			he_wi = '80px';
-		}
-	} else if(no_of_grid >5 && no_of_grid <= 10) {
-		if(screen.width <= 424) {
-			if(no_of_grid > 8) {
-				alert("8 is the Max limit to create Grid! (for your device)");
-				return;
-			}
-			//console.log("everything good here! 424");
-			he_wi = '35px';
-		} else if(screen.width <= 480) {
-			he_wi = '30px';
-			//console.log("everything good here! 480");
-		} else if(screen.width <= 770) {
-			he_wi = '40px';
-			//console.log("everything good here! 770");
-		} else {
-			//console.log("everything good here! else");
-			he_wi = '50px';
-		}
-	}		     
-
-	//console.log(players);
-	// hide form
-	document.body.style.backgroundImage = 'none';
-	document.getElementById('form_container').setAttribute('class', 'hide');
-
-	createCells();
-	createBoard();
-
-	document.getElementById('turn').innerHTML = players[turn%2];
-	document.getElementById('player_1').disabled = 'true';
-	document.getElementById('player_2').disabled = 'true';
-	document.getElementById('no_of_grid').disabled = 'true';
-	document.getElementById('start_btn').disabled = 'true'; 
-	document.getElementById('hide_section').className = '';
-	console.log("Calling Computer function");
-};
 
 const isWinner = () => {
-	// first check for all rows in board and then for col and then for diagonals
-	let len = board.length;
-	if(turn < len) {
-		return false;
-	}
-	console.log(board.length);
+  // first check for all rows in board and then for col and then for diagonals
+  let len = board.length;
+  if (turn < len) {
+    return false;
+  }
+  // console.log(board.length);
 
-	for(let i=0; i<len; i++) {
+  for (let i = 0; i < len; i++) {
+    if (board[i].every((el) => el === board[i][0] && el !== "")) {
+      return true;
+    }
 
-		if(board[i].every((el) => el === board[i][0] && el !== "")) {
-			return true;
-		}
-		//console.log(`${i} Row clear`);
-		let start_col_val = board[0][i];
-		let count = 1;
-		for(let j=1; j<len; j++) {
-			if(start_col_val === board[j][i] && start_col_val !== "") {
-				count++;
-			}
-		}
-		//console.log(`${i} Col clear`);
-		if(count === len) {
-			return true;
-		}
-	}
+    //console.log(`${i} Row clear`);
+    let start_col_val = board[0][i];
+    let count = 1;
+    for (let j = 1; j < len; j++) {
+      if (start_col_val === board[j][i] && start_col_val !== "") {
+        count++;
+      }
+    }
+    //console.log(`${i} Col clear`);
+    if (count === len) {
+      return true;
+    }
+  }
 
-	// check for diagonal
+  // check for diagonal (Left)
 
-	let i = board[0][0];
-	let j=0;
-	while(j < len) {
-		
-		if(board[0][0] === "") {
-			break;
-		}
-		if(board[j][j] !== i) {
-			break;
-		} else {
-			j++;
-		}
-	}
-	
-	if(j === len) {
-		return true;
-	}
+  let i = board[0][0];
+  let j = 0;
+  while (j < len) {
+    if (board[0][0] === "") {
+      break;
+    }
+    if (board[j][j] !== i) {
+      break;
+    } else {
+      j++;
+    }
+  }
 
-	let rev_i = 0
-	let rev_j = len - 1;
-	let rev_val = board[rev_i][rev_j];
+  if (j === len) {
+    return true;
+  }
 
-	while(rev_i < len) {
-		if(board[rev_i][rev_j] === "") {
-			break;
-		}
-		if(rev_val !== board[rev_i][rev_j]) {
-			break;
-		} else {
-			rev_i++;
-			rev_j--;
-		}
-	}
-	//console.log(`reverse Diagonal clear`);
-	if(rev_i === len) {
-		return true;
-	}
+  // check for diagonal (right)
+  let rev_i = 0;
+  let rev_j = len - 1;
+  let rev_val = board[rev_i][rev_j];
 
-	return false;
+  while (rev_i < len) {
+    if (board[rev_i][rev_j] === "") {
+      break;
+    }
+    if (rev_val !== board[rev_i][rev_j]) {
+      break;
+    } else {
+      rev_i++;
+      rev_j--;
+    }
+  }
+  //console.log(`reverse Diagonal clear`);
+  if (rev_i === len) {
+    return true;
+  }
+
+  return false;
 };
 
 let gameStatus = document.getElementById('game_status');
